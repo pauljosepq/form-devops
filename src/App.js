dacc2e84b39d69435828db2cc33ffa2bcd875710
Paul Paniagua <pauljosepq2@gmail.com>
2023-07-17 18:04:03 -0400

    add base project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import {useState} from "react";
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleSignIn = () => {
+
+    // Save the email, hashed password, and remember me status in the local storage
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+    localStorage.setItem('rememberMe', rememberMe.toString());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" onChange={(e) => setEmail(e.target.value)}/>
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" onChange={(e) => setPassword(e.target.value)}/>
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} >
+                  Remember me
+                </Checkbox>
+                <Link color={'blue.400'}>Forgot password?</Link>
+              </Stack>
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}
+                onClick={handleSignIn}
+              >
+                Sign in
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,66 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent, screen } from '@testing-library/react';
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Unit Test', () => {
+  test('email state variable updates on input change', () => {
+    const {getByLabelText} = render(<App/>);
+    const emailInput = getByLabelText('Email address');
+
+    fireEvent.change(emailInput, {target: {value: 'test@example.com'}});
+
+    expect(emailInput.value).toBe('test@example.com');
+  });
+
+  test('password state variable updates on input change', () => {
+    const {getByLabelText} = render(<App/>);
+    const passwordInput = getByLabelText('Password');
+
+    fireEvent.change(passwordInput, {target: {value: 'testpassword'}});
+
+    expect(passwordInput.value).toBe('testpassword');
+  });
+
+  test('rememberMe state variable updates on checkbox toggle', () => {
+    const {getByLabelText} = render(<App/>);
+    const rememberMeCheckbox = getByLabelText('Remember me');
+
+    fireEvent.click(rememberMeCheckbox);
+
+    expect(rememberMeCheckbox.checked).toBe(true);
+  });
 });
+describe('End-To-End test', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  afterEach(() => {
+    localStorage.clear();
+  });
+
+  test('should save email, password, and remember me in local storage when signing in', () => {
+    const emailInput = screen.getByLabelText('Email address');
+    const passwordInput = screen.getByLabelText('Password');
+    const rememberMeCheckbox = screen.getByLabelText('Remember me');
+    const signInButton = screen.getByText('Sign in');
+
+    // Fill in the form inputs
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+    fireEvent.click(rememberMeCheckbox);
+
+    // Click the sign-in button
+    fireEvent.click(signInButton);
+
+    // Retrieve values from local storage
+    const savedEmail = localStorage.getItem('email');
+    const savedPassword = localStorage.getItem('password');
+    const savedRememberMe = localStorage.getItem('rememberMe');
+
+    // Assert that the values were saved correctly
+    expect(savedEmail).toBe('test@example.com');
+    expect(savedPassword).toBeDefined();
+    expect(savedRememberMe).toBe('true');
+  });
+});
+
